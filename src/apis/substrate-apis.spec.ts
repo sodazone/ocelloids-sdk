0aeac5261e1fb9375ec18c0aece77f7a5465cbe0
Marc Fornos <marc.fornos@gmail.com>
2023-06-12 10:46:38 +0200

    base project layout

---
+++ src/apis/substrate-apis.spec.ts
@@ -0,0 +1,5 @@
+import { SubstrateApis } from './';
+
+test('instantiate', () => {
+  expect(new SubstrateApis()).toBeDefined();
+});
---
+++ src/apis/substrate-apis.ts
@@ -0,0 +1,7 @@
+class SubstrateApis {
+
+}
+
+export {
+  SubstrateApis
+};
