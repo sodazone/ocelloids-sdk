6085329771008fe50a923872d25ed8a0e633b813
Xueying Wang <wang.xy.326@gmail.com>
2023-06-13 12:07:18 +0200

    substrate API tests with mock WS provider

--- src/apis/substrate-apis.spec.ts
+++ src/apis/substrate-apis.spec.ts
@@ -1,10 +1,52 @@
+jest.mock('@polkadot/api', () => {
+  const original = jest.requireActual('@polkadot/api');
+
+  return {
+    ...original,
+    WsProvider: jest.fn().mockImplementation(() => {
+      return {
+        hasSubscriptions: jest.fn(() => {return true;}),
+        on: jest.fn(),
+        connect: jest.fn(),
+        disconnect: jest.fn(),
+        send: jest.fn(),
+        subscribe: jest.fn(),
+        unsubscribe: jest.fn()
+      }; })
+  };
+});
+
 import { configuration } from '../index.js';
 import { SubstrateApis } from './substrate-apis.js';
 
-test('instantiate', async () => {
-  //* TODO: mock ws
-  const apis = new SubstrateApis(configuration);
-  console.log(apis.promise.polkadot.isReady);
-  expect(apis).toBeDefined();
-  //*/
+describe('substrate APIs', () => {
+  let apis: SubstrateApis;
+
+  beforeAll(() => {
+    apis = new SubstrateApis(configuration);
+  });
+
+  test('instantiate', () => {
+    expect(apis).toBeDefined();
+    expect(apis.promise).toBeDefined();
+    expect(apis.rx).toBeDefined();
+    expect(apis.promise.polkadot).toBeDefined();
+    expect(apis.rx.polkadot).toBeDefined();
+    expect(apis.promise.rococo).toBeDefined();
+    expect(apis.rx.rococo).toBeDefined();
+  });
+
+  test('disconnect', async () => {
+    // Cast as any to access private readonly property
+    const polkadotProvider = (apis as any).providers['polkadot'] as any;
+    const rococoProvider = (apis as any).providers['rococo'] as any;
+    expect(polkadotProvider).toBeDefined();
+    expect(rococoProvider).toBeDefined();
+
+    // Call disconnect on all providers
+    await apis.disconnect();
+
+    expect(polkadotProvider.disconnect).toBeCalledTimes(1);
+    expect(rococoProvider.disconnect).toBeCalledTimes(1);
+  });
 });
