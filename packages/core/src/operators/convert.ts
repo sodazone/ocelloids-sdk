d865f06dc777096f092c569081a54068a2f37844
Marc Fornos <marc.fornos@gmail.com>
2023-06-28 16:36:40 +0200

    update converter doc

--- packages/core/src/operators/convert.ts
+++ packages/core/src/operators/convert.ts
@@ -5,8 +5,9 @@ import { Converter, base } from '../converters/index.js';
  * Maps the values emitted by the source observable to their named primitive representation.
  *
  * @typeparam T The type of values emitted by the source observable.
+ * @param converter The converter to use, defaults to the base converter.
  * @returns A function that takes an observable source and returns an observable that emits the named primitive values.
- * @see {@link toNamedPrimitive}
+ * @see {@link Converter}
  */
 export function convert<T>(converter: Converter = base) {
   return (source: Observable<T>) => {
