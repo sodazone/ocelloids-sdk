b441601762365acc8c60d41700aad138d2ac9af6
Marc Fornos <marc.fornos@gmail.com>
2023-06-28 13:24:47 +0200

    add query change test

--- packages/core/src/subjects/query.spec.ts
+++ packages/core/src/subjects/query.spec.ts
@@ -44,4 +44,37 @@ describe('control query', () => {
 
     expect(q.getValue().test(data)).toBeTruthy();
   });
+
+  it('should be able to change the query value', () => {
+    const data = [{
+      method: 'transferAllowDeath',
+      section: 'balances',
+      args: {
+        dest: { id: '14NEHDwc5PPQfEjzLVDbVbi4djQLQZ9u7mMU3BPhTFJf4cD6' },
+        value: '108515280000000000'
+      }
+    },
+    {
+      method: 'transferAllowDeath',
+      section: 'balances',
+      args: {
+        dest: { id: '14NEHDwc5PPQfEjzLVDbVbi4djQLQZ9u7mMU3BPhTFJf4cD6' },
+        value: '108515280000000001'
+      }
+    }];
+
+    const q = ControlQuery.from({
+      'args.value': { $bn_lt: '108515280000000001' }
+    });
+
+    expect(q.getValue().test(data[0])).toBeTruthy();
+    expect(q.getValue().test(data[1])).toBeFalsy();
+
+    q.change({
+      'args.value': { $bn_gt: '108515280000000000' }
+    });
+
+    expect(q.getValue().test(data[0])).toBeFalsy();
+    expect(q.getValue().test(data[1])).toBeTruthy();
+  });
 });
