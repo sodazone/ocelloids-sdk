49e5973cd4126a5a5aa899ec36cc93466919859a
Marc Fornos <marc.fornos@gmail.com>
2023-06-28 13:26:01 +0200

    add filter tests

---
+++ packages/core/src/operators/filter.spec.ts
@@ -0,0 +1,46 @@
+import { from } from 'rxjs';
+
+import { filterEvents } from './filter.js';
+
+import { testBlocks, testExtrinsics, testEvents } from '@sodazone/ocelloids-test';
+import { EventWithId } from '../types/interfaces.js';
+
+describe('filterEvents', () => {
+  it('should emit events with contextual information', done => {
+    const calls = jest.fn();
+    const testPipe = filterEvents({
+      section: { $exists: true }
+    })(from(testBlocks));
+    testPipe.subscribe({
+      next: (event: EventWithId) => {
+        calls();
+        expect(event).toBeDefined();
+        expect(event.blockNumber).toBeDefined();
+      },
+      complete: () => {
+        expect(calls).toBeCalledTimes(148);
+        done();
+      }
+    });
+  });
+  it('should allow to apply an extrinsic level criteria', done => {
+    const calls = jest.fn();
+    const testPipe = filterEvents({
+      section: { $exists: true }
+    },
+    {
+      'extrinsic.isSigned': true
+    })(from(testBlocks));
+    testPipe.subscribe({
+      next: (event: EventWithId) => {
+        calls();
+        expect(event).toBeDefined();
+        expect(event.blockNumber).toBeDefined();
+      },
+      complete: () => {
+        expect(calls).toBeCalledTimes(32);
+        done();
+      }
+    });
+  });
+});
