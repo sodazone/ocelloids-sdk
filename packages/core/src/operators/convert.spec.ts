9d26497bf7496bbab9a339e4dfbf90497dc597b4
Marc Fornos <marc.fornos@gmail.com>
2023-07-04 13:08:51 +0200

    add convert operator test

---
+++ packages/core/src/operators/convert.spec.ts
@@ -0,0 +1,16 @@
+import { from } from 'rxjs';
+
+import { testBlocks } from '@sodazone/ocelloids-test';
+import { convert } from './convert.js';
+
+describe('convert operator', () => {
+  it('should convert an extended signed block', done => {
+    convert()(from([testBlocks[0]]))
+      .subscribe((c: any) => {
+        expect(c).toBeDefined();
+        expect(c.block.header.extrinsicsRoot)
+          .toBe('0x382951a7547ec688051e1d95c0589eb8bd247bd4451cf66af35cdfee0f674692');
+        done();
+      });
+  });
+});
