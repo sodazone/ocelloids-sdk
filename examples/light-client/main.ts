ef79607af35f32cb6555d469e9d5301a83b34041
Marc Fornos <marc.fornos@gmail.com>
2024-01-17 09:19:09 +0100

    use SPDX license id

--- examples/light-client/main.ts
+++ examples/light-client/main.ts
@@ -1,20 +1,7 @@
 #!/usr/bin/env ts-node-esm
 
-/*
- * Copyright 2023-2024 SO/DA zone ~ Marc Fornós & Xueying Wang
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2023-2024 SO/DA zone
+// SPDX-License-Identifier: Apache-2.0
 
 import { ScProvider } from '@polkadot/rpc-provider/substrate-connect';
 import * as Sc from '@substrate/connect';
