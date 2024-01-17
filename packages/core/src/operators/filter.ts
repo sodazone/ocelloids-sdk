60f4deef531f4a7ebc6b3ca1cf79a187e9fe2a28
Xueying Wang <wang.xy.326@gmail.com>
2024-01-17 13:31:39 +0100

    make flatten optional in filterExtrinsics and filterEvents

--- packages/core/src/operators/filter.ts
+++ packages/core/src/operators/filter.ts
@@ -14,46 +14,29 @@ import { EventWithId, TxWithIdAndEvent } from '../types/interfaces.js';
 /**
  * Filters extrinsics based on the provided criteria.
  *
- * It extracts extrinsics with events from the given block,
- * flattens batch calls if needed and applies the filter criteria.
+ * This function extracts extrinsics with events from the given block,
+ * optionally flattens batch calls, and applies the filter criteria.
  *
- * @param extrinsicsCriteria The criteria to filter extrinsics.
- * @returns An observable that emits filtered extrinsics with identifier and event information.
- */
-export function filterExtrinsicsFlattened(
-  extrinsicsCriteria: Criteria
-) {
-  return (source: Observable<SignedBlockExtended>)
-    : Observable<TxWithIdAndEvent> => {
-    return source.pipe(
-      // Extracts extrinsics with events
-      extractTxWithEvents(),
-      // Flattens batches/multisig/proxy/derivative calls recursively
-      flattenCalls(),
-      // Filters at the extrinsic level
-      // mainly for success or failure
-      mongoFilter(extrinsicsCriteria)
-    );
-  };
-}
-
-/**
- * Filters extrinsics based on the provided criteria.
- *
- * It extracts extrinsics with events from the given block,
- * flattens batch calls if needed and applies the filter criteria.
- *
- * @param extrinsicsCriteria The criteria to filter extrinsics.
+ * @param extrinsicsCriteria - The criteria to filter extrinsics.
+ * @param flatten - (Optional) A flag indicating whether to flatten nested calls. Defaults to true.
  * @returns An observable that emits filtered extrinsics with identifier and event information.
  */
 export function filterExtrinsics(
-  extrinsicsCriteria: Criteria
+  extrinsicsCriteria: Criteria,
+  flatten: boolean = true
 ) {
   return (source: Observable<SignedBlockExtended>)
     : Observable<TxWithIdAndEvent> => {
     return source.pipe(
       // Extracts extrinsics with events
       extractTxWithEvents(),
+      // If the flatten flag is set to true,
+      // pass through flattenCalls operator to recursively flatten
+      // nested batch, multisig, proxy, etc. calls.
+      // Else, pass through.
+      flatten ?
+        flattenCalls() :
+        x => x,
       // Filters at the extrinsic level
       // mainly for success or failure
       mongoFilter(extrinsicsCriteria)
@@ -64,22 +47,30 @@ export function filterExtrinsics(
 /**
  * Returns an Observable that emits events filtered based on the provided criteria.
  *
+ * This method allows filtering events based on both event-level criteria and extrinsic-level criteria.
+ * Optionally, it flattens nested calls in extrinsics and applies the filtering criteria.
+ *
  * @param eventsCriteria - Criteria for filtering events.
  * @param extrinsicsCriteria - (Optional) Criteria for filtering extrinsics. Defaults to `{ dispatchError: { $exists: false } }`.
- * @returns An Observable that emits EventWithId objects that meet the filtering criteria.
+ * @param flatten - (Optional) A flag indicating whether to flatten nested calls.
+ * Defaults to false to avoid duplication of filtered events.
+ * When extrinsicCriteria is set for filtering of calls by method and section,
+ * it is advisable to set the flatten flag to true to not miss nested calls.
+ * @returns An Observable that emits `EventWithId` objects that meet the specified filtering criteria.
  */
 export function filterEvents(
   eventsCriteria: ControlQuery | Criteria,
   extrinsicsCriteria : Criteria = {
     dispatchError: { $exists: false }
-  }
+  },
+  flatten: boolean = false
 ) {
   const eventsQuery = ControlQuery.from(eventsCriteria);
 
   return (source: Observable<SignedBlockExtended>)
     : Observable<EventWithId> => {
     return source.pipe(
-      filterExtrinsics(extrinsicsCriteria),
+      filterExtrinsics(extrinsicsCriteria, flatten),
       // Maps the events with
       // block and extrinsic context
       extractEventsWithTx(),
